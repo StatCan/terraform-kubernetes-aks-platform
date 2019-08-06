179b8a220fb3c38c78ced5b3aafffd6cef0c93cd
William Hearn <sylus1984@gmail.com>
2019-08-05 20:24:38 -0400

    feat(drupal): Disable by default

--- main.tf
+++ main.tf
@@ -76,47 +76,47 @@ module "namespace_ci" {
 
 # Drupal
 
-resource "kubernetes_namespace" "drupal" {
-  metadata {
-    name = "drupal"
+# resource "kubernetes_namespace" "drupal" {
+#   metadata {
+#     name = "drupal"
 
-    labels = {
-      istio-injection = "enabled"
-    }
-  }
-}
+#     labels = {
+#       istio-injection = "enabled"
+#     }
+#   }
+# }
 
-module "namespace_drupal" {
-  source = "git::https://github.com/statcan/terraform-kubernetes-namespace.git"
+# module "namespace_drupal" {
+#   source = "git::https://github.com/statcan/terraform-kubernetes-namespace.git"
 
-  name = "${kubernetes_namespace.drupal.metadata.0.name}"
-  namespace_admins = {
-    users = []
-    groups = [
-      "${var.kubernetes_rbac_group}"
-    ]
-  }
+#   name = "${kubernetes_namespace.drupal.metadata.0.name}"
+#   namespace_admins = {
+#     users = []
+#     groups = [
+#       "${var.kubernetes_rbac_group}"
+#     ]
+#   }
 
-  # ServiceAccount
-  helm_service_account = "tiller"
+#   # ServiceAccount
+#   helm_service_account = "tiller"
 
-  # ServiceQuota Overrides
-  allowed_loadbalancers = "10"
-  allowed_nodeports = "10"
+#   # ServiceQuota Overrides
+#   allowed_loadbalancers = "10"
+#   allowed_nodeports = "10"
 
-  # CICD
-  ci_name = "argo"
+#   # CICD
+#   ci_name = "argo"
 
-  # Image Pull Secret
-  # kubernetes_secret = "${var.kubernetes_secret}"
-  # docker_repo = "${var.docker_repo}"
-  # docker_username = "${var.docker_username}"
-  # docker_password = "${var.docker_password}"
-  # docker_email = "${var.docker_email}"
-  # docker_auth = "${var.docker_auth}"
+#   # Image Pull Secret
+#   # kubernetes_secret = "${var.kubernetes_secret}"
+#   # docker_repo = "${var.docker_repo}"
+#   # docker_username = "${var.docker_username}"
+#   # docker_password = "${var.docker_password}"
+#   # docker_email = "${var.docker_email}"
+#   # docker_auth = "${var.docker_auth}"
 
-  dependencies = []
-}
+#   dependencies = []
+# }
 
 # ElasticSearch
 
