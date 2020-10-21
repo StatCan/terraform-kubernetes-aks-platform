2aa1a54edd00bad5d5376393cc39ac3f0e30aeca
William Hearn <sylus1984@gmail.com>
2020-10-20 23:07:52 -0400

    feat(sync): Sync with SDLC

---
+++ namespace_vault.tf
@@ -0,0 +1,45 @@
+# vault
+
+resource "kubernetes_namespace" "vault" {
+  metadata {
+    name = "vault"
+
+    annotations = {
+      "logging.csp.vmware.com/fluentd-status" = ""
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata.0.annotations["logging.csp.vmware.com/fluentd-status"]
+    ]
+  }
+}
+
+module "namespace_vault" {
+  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-namespace.git?ref=v1.0.1"
+  name = "${kubernetes_namespace.vault.metadata.0.name}"
+  namespace_admins = {
+    users = []
+    groups = [
+      "${var.kubernetes_rbac_group}"
+    ]
+  }
+
+  # ServiceAccount
+  helm_service_account = "tiller"
+
+  # CICD
+  ci_name = "deploy"
+
+  # Image Pull Secret
+  enable_kubernetes_secret = "${var.enable_kubernetes_secret}"
+  kubernetes_secret        = "${var.kubernetes_secret}"
+  docker_repo              = "${var.docker_repo}"
+  docker_username          = "${var.docker_username}"
+  docker_password          = "${var.docker_password}"
+  docker_email             = "${var.docker_email}"
+  docker_auth              = "${var.docker_auth}"
+
+  dependencies = []
+}
